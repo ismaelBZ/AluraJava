0b4e05c940a0b4215edf427d614b48915ad40ec5
Ismael <ismaelbetzoletti@gmail.com>
2023-07-19 13:47:39 -0300

    added Errors section in aula02.intFloatDoubleCast

--- aula02_understandingNumbers/IntFloatDoubleCast.java
+++ aula02_understandingNumbers/IntFloatDoubleCast.java
@@ -17,10 +17,6 @@ public class IntFloatDoubleCast {
         int castedNumber = (int) 4.0; // 4.0 is a double and is casted in an integer
         System.out.println(castedNumber);
 
-        //Operation Error
-        double division = 5 / 2;
-        System.out.println(division); // That occurs because java realize the two integers division and return and casted integer as a division too, after that value is assigned to the double variable
-
         // Java recognize floating numbers as doubles, so need to cast them or describe
         // it with letter f after the number
         float pi = (float) 3.14159;
@@ -28,12 +24,22 @@ public class IntFloatDoubleCast {
         float aceleracaoDaGravidadeEmMetrosPorSegundo = 9.80665f; // Look at the f letter after number
         System.out.println("A aceleraçao da gravidade da terra é: " + aceleracaoDaGravidadeEmMetrosPorSegundo);
 
+        
+        /*  ERRORS  ERRORS  ERRORS */
+
+        //Compile Error
+        // int integerValue = 1.0  // It will not compile because java can't storage a double value (8 bytes) in an integer type (4 bytes)
+
+        //Operation Error
+        double division = 5 / 2;
+        System.out.println(division); // That occurs because java realize the two integers division and return and casted integer as a division too, after that value is assigned to the double variable
+
         // Errors on numbers
         double zeroOne = 0.1;
         double zeroTwo = 0.2;
         double notZeroThree = zeroOne + zeroTwo;
         System.out.println("A utilização do tipo double em soma pode gerar um erro: " + notZeroThree);
 
-    }
+    }   
 
 }
