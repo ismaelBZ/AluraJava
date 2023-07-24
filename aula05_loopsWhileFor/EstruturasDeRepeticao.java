319fa205e0a3f3ec7a83a840c06bcbd1d6e05618
Ismael <ismaelbetzoletti@gmail.com>
2023-07-24 15:20:52 -0300

    added a factorial calculator for loop

--- aula05_loopsWhileFor/EstruturasDeRepeticao.java
+++ aula05_loopsWhileFor/EstruturasDeRepeticao.java
@@ -27,5 +27,19 @@ public class EstruturasDeRepeticao {
         for (int number = 3; number < 100; number = number + 3) {
             System.out.print(number + " ");
         }
+
+        System.out.println();
+        
+        int factorial = 1;
+        int whatFactorial = 6;
+        if (whatFactorial == 0) {
+            factorial = 1;
+        } else {
+            for (int iterator = 1; iterator <= whatFactorial; iterator++) {
+                    factorial *= iterator;
+                    System.out.println(factorial);
+            }
+        }
+        System.out.println(factorial);
     }
 }
