9f5841a0d8a6fa3d783ff8f730c5a1735fb66d2e
Ismael <ismaelbetzoletti@gmail.com>
2023-08-02 10:58:40 -0300

    Understanding object referencing through variables

--- bytebank/bytebank/src/TestaConta.java
+++ bytebank/bytebank/src/TestaConta.java
@@ -1,9 +1,17 @@
 public class TestaConta{
     public static void main (String[] args) {
+        
+        // Replace default attribute value
         Conta firstAccount = new Conta();
         System.out.println(firstAccount.agency);
         firstAccount.agency = 623;
-        System.out.println(firstAccount.agency); // Replace Default Value
+        System.out.println(firstAccount.agency); 
+
+        // Understanding object referencing through variables
+        Conta secondAccount = firstAccount;
+        System.out.println(firstAccount);  // Is the same reference, so is the same object
+        System.out.println(secondAccount); // Is the same reference, so is the same object
+        
     }
 
 }
